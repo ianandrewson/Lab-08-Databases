1bee5d0aa3b6892e88e066f54227759d66ad95af
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-07 10:09:30 -0800

    added locations

--- data/buildingdata.js
+++ data/buildingdata.js
@@ -2,6 +2,7 @@ module.exports = [{
     'name': 'Abraj Al-Bait Clock Tower',
     'built': 2012,
     'is home?': true,
+    'Location': 'Mecca, Saudi Arabia',
     'url': 'https://en.wikipedia.org/wiki/Abraj_Al_Bait#/media/File:Abraj-al-Bait-Towers.JPG',
     'height': '120 floors'
 },
@@ -9,6 +10,7 @@ module.exports = [{
     'name': 'Einsteinhaus',
     'built': '1700s',
     'is home?': true,
+    'Location': 'Berne, Germany',
     'url': 'https://en.wikipedia.org/wiki/Einsteinhaus#/media/File:Albert_einstein_house_bern.JPG',
     'height': '3 floors'
 },
@@ -16,6 +18,7 @@ module.exports = [{
     'name': 'Palais Garnier',
     'built': '1861-1875',
     'is home?': false,
+    'Location': 'Paris, France',
     'url': 'https://en.wikipedia.org/wiki/Palais_Garnier#/media/File:Op%C3%A9ra_Garnier_-_le_Grand_Foyer.jpg',
     'height': '184ft',
 },
@@ -23,6 +26,7 @@ module.exports = [{
     'name': 'Wyndham New Yorker Hotel',
     'built': '1930',
     'is home?': true,
+    'Location': 'New York, New York',
     'url': 'https://en.wikipedia.org/wiki/Wyndham_New_Yorker_Hotel#/media/File:NewYorker_Hotel.JPG',
     'height': '43 floors',
 },
@@ -30,6 +34,7 @@ module.exports = [{
     'name': 'Strasbourg Cathedral',
     'built': '1015-1439',
     'is home?': false,
+    'Location': 'Strasbourg, France',
     'url': 'https://en.wikipedia.org/wiki/Strasbourg_Cathedral#/media/File:Strasbourg_Cathedral_Exterior_-_Diliff.jpg',
     'height': '466 ft',
 }];
