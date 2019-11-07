3d2d8b3be0b1b62c582608394d7b3329e9fb0b22
Ian Andrewson <ianandrewson@gmail.com>
2019-11-07 10:18:22 -0800

    half of server.js set up

--- server.js
+++ server.js
@@ -1,2 +1,17 @@
 require('dotenv').config();
 
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const pg = require('pg');
+
+const Client = pg.Client;
+const client = new Client(process.env.DATABASE_URL);
+client.connect();
+
+const app = express();
+const PORT = process.env.PORT;
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.static('public'));
+
