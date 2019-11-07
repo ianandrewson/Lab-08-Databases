a8ce4ba2c44d4df43339df7d9e5444596a18b3e2
Ian Andrewson <ianandrewson@gmail.com>
2019-11-07 11:19:31 -0800

    finished server file

--- server.js
+++ server.js
@@ -15,3 +15,28 @@ app.use(morgan('dev'));
 app.use(cors());
 app.use(express.static('public'));
 
+app.get('/api/buildings', async(req, res) => {
+    try {
+        const result = await client.query(`
+            SELECT
+                id,
+                name,
+                built,
+                is_home,
+                location,
+                url,
+                height
+            FROM buildings;
+        `);
+        res.json(result.rows);
+    }
+    catch (err) {
+        res.status(500).json({
+            error: err.message || err
+        });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log('server runnong on port ' + PORT);
+})
