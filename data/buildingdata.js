d8474ac3b7d4dc78583a0a67bdb90988d016f316
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-07 10:05:27 -0800

    made buildings

---
+++ data/buildingdata.js
@@ -0,0 +1,35 @@
+module.exports = [{
+    'name': 'Abraj Al-Bait Clock Tower',
+    'built': 2012,
+    'is home?': true,
+    'url': 'https://en.wikipedia.org/wiki/Abraj_Al_Bait#/media/File:Abraj-al-Bait-Towers.JPG',
+    'height': '120 floors'
+},
+{
+    'name': 'Einsteinhaus',
+    'built': '1700s',
+    'is home?': true,
+    'url': 'https://en.wikipedia.org/wiki/Einsteinhaus#/media/File:Albert_einstein_house_bern.JPG',
+    'height': '3 floors'
+},
+{
+    'name': 'Palais Garnier',
+    'built': '1861-1875',
+    'is home?': false,
+    'url': 'https://en.wikipedia.org/wiki/Palais_Garnier#/media/File:Op%C3%A9ra_Garnier_-_le_Grand_Foyer.jpg',
+    'height': '184ft',
+},
+{
+    'name': 'Wyndham New Yorker Hotel',
+    'built': '1930',
+    'is home?': true,
+    'url': 'https://en.wikipedia.org/wiki/Wyndham_New_Yorker_Hotel#/media/File:NewYorker_Hotel.JPG',
+    'height': '43 floors',
+},
+{
+    'name': 'Strasbourg Cathedral',
+    'built': '1015-1439',
+    'is home?': false,
+    'url': 'https://en.wikipedia.org/wiki/Strasbourg_Cathedral#/media/File:Strasbourg_Cathedral_Exterior_-_Diliff.jpg',
+    'height': '466 ft',
+}];
