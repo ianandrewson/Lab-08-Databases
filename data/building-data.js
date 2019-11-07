6eac6d297f8680e10bb4aeddacadc7329aa932ed
Ian Andrewson <ianandrewson@gmail.com>
2019-11-07 10:13:34 -0800

    merged data into single file

--- data/building-data.js
+++ data/building-data.js
@@ -1,4 +1,4 @@
-const buildings = [
+module.exports = [
     {
         'Name': 'Woolsthorpe Manor',
         'Built': '17th century',
@@ -38,5 +38,45 @@ const buildings = [
         'Location': 'Novato, California',
         'URL': '../public/assets/hildhood_home.jpg',
         'Height': '2.5 stories'
+    },
+    { 
+        'name': 'Abraj Al-Bait Clock Tower',
+        'built': 2012,
+        'is home?': true,
+        'Location': 'Mecca, Saudi Arabia',
+        'url': 'https://en.wikipedia.org/wiki/Abraj_Al_Bait#/media/File:Abraj-al-Bait-Towers.JPG',
+        'height': '120 floors'
+    },
+    {
+        'name': 'Einsteinhaus',
+        'built': '1700s',
+        'is home?': true,
+        'Location': 'Berne, Germany',
+        'url': 'https://en.wikipedia.org/wiki/Einsteinhaus#/media/File:Albert_einstein_house_bern.JPG',
+        'height': '3 floors'
+    },
+    {
+        'name': 'Palais Garnier',
+        'built': '1861-1875',
+        'is home?': false,
+        'Location': 'Paris, France',
+        'url': 'https://en.wikipedia.org/wiki/Palais_Garnier#/media/File:Op%C3%A9ra_Garnier_-_le_Grand_Foyer.jpg',
+        'height': '184ft',
+    },
+    {
+        'name': 'Wyndham New Yorker Hotel',
+        'built': '1930',
+        'is home?': true,
+        'Location': 'New York, New York',
+        'url': 'https://en.wikipedia.org/wiki/Wyndham_New_Yorker_Hotel#/media/File:NewYorker_Hotel.JPG',
+        'height': '43 floors',
+    },
+    {
+        'name': 'Strasbourg Cathedral',
+        'built': '1015-1439',
+        'is home?': false,
+        'Location': 'Strasbourg, France',
+        'url': 'https://en.wikipedia.org/wiki/Strasbourg_Cathedral#/media/File:Strasbourg_Cathedral_Exterior_-_Diliff.jpg',
+        'height': '466 ft',
     }
-]
+];
