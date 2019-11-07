8c1f537b860f699b1172e3a11a751c54aa44d7cd
Ian Andrewson <ianandrewson@gmail.com>
2019-11-07 10:01:16 -0800

    5 items of data from Ian

---
+++ data/building-data.js
@@ -0,0 +1,42 @@
+const buildings = [
+    {
+        'Name': 'Woolsthorpe Manor',
+        'Built': '17th century',
+        'Is home?': true,
+        'Location': 'Grantham, Lincolnshire, England',
+        'URL': '../public/assets/Woolsthorpe_Manor.jpg',
+        'Height': '3 stories'
+    },
+    {
+        'Name': 'Amalienborg',
+        'Built': '1760',
+        'Is home?': true,
+        'Location': 'Copenhagen, Denmark',
+        'URL': '../public/assets/Amalienborg.jpg',
+        'Height': '4 stories'
+    },
+    {
+        'Name': 'Sydney Opera House',
+        'Built': 'Oct 20th 1973',
+        'Is home?': false,
+        'Location': 'Sydney, Australia',
+        'URL': '../public/assets/sydney_opera_house.jpg',
+        'Height': '213 ft'
+    },
+    {
+        'Name': 'Bilbo Baggins\'s House',
+        'Built': 'TA 2889',
+        'Is home?': true,
+        'Location': 'Bagshot Row, Hobbiton',
+        'URL': '../public/assets/baggins.png',
+        'Height': '1 story'
+    },
+    {
+        'Name': 'Ian\'s Childhood Home',
+        'Built': '1963',
+        'Is Home?': true,
+        'Location': 'Novato, California',
+        'URL': '../public/assets/hildhood_home.jpg',
+        'Height': '2.5 stories'
+    }
+]
