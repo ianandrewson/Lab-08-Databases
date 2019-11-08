ef8fe476b7c94744958b0ddf4f417e7e4d1e3fcb
Alan Hermanns <alan.hermanns.the.first@gmail.com>
2019-11-08 12:02:24 -0800

    form start

--- public/resource-form/formApp.js
+++ public/resource-form/formApp.js
@@ -1,16 +1,34 @@
 import Component from '../Component.js';
 import Header from '../common/Header.js';
-
+import getIsHome from '../services/domain-api.js';
 class App extends Component {
-    onRender(dom){
+    async onRender(dom){
         const header = new Header();
         const headerDOM = header.renderDOM();
         dom.prepend(headerDOM);
+        const isHomeValues = getIsHome();
+        let isHomePartOfForm = dom.getElementsByClassName('is-home');
+        dom.createElement('option');
     }
-    renderHTML(){
-        return /*html*/`
-            <p>test</p>
+    async renderHTML(){
+        const dom = /*html*/`
+            <form>
+                <label>Building Name</label>
+                    <input type = 'text'>
+                <label>Built</label>
+                    <input type = 'text'>
+                <label class = 'is-home'>Is it a home?</label>
+                
+                <label>Location</label>
+                    <input type = 'text'>
+                <label>Link to image</label>
+                    <input type ='text'>
+                <label>Height</label>
+                    <input type = 'text'>
+                <button type = 'submit'>LOAD UP</button> 
+            </form>
         `;
+        return dom;
     }
 
 }
--- public/services/domain-api.js
+++ public/services/domain-api.js
@@ -8,3 +8,11 @@ export async function getBuildings() {
     console.log(data);
     return data;
 }
+
+export async function getIsHome(){
+    const url = `${URL}/ishome`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
