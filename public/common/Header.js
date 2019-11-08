ad635d806efb5b52f82d9fc19dd43f7c6246d23b
Ian Andrewson <ianandrewson@gmail.com>
2019-11-07 17:54:04 -0800

    added link to home

--- public/common/Header.js
+++ public/common/Header.js
@@ -5,6 +5,7 @@ class Header extends Component {
     renderHTML() {
         return /*html*/`
         <nav>
+            <a href="../index.html">Home</a>
             <a href="../resource-list.html">Buildings</a>
             <a href="../resource-form.html">Add a Building</a>
         </nav>
